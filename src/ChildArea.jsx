c010dac470b6915265aa90fb79a266f524603b40
git-morioka <86875615+git-morioka@users.noreply.github.com>
2022-07-02 17:50:44 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+
+const style = {
+  with: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  const data = [...Array(2000).keys()];
+  console.log(data);
+  data.forEach(() => {
+    console.log("test");
+  });
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
